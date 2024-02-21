fe03e7ae0c2a55dd066374eb304d8dfcb1d1ba1e
kunal1516 <147371834+kunal1516@users.noreply.github.com>
2024-02-21 18:11:36 +0530

    forgot pass done

--- router/InternRoute.js
+++ router/InternRoute.js
@@ -1,16 +1,20 @@
 const express = require('express')
 const router = express.Router()
 
-const { signUp, login, getAll, gets, updateIntern, deleteIntern, updatePassword, handleRefreshToken } = require('../controller/internCntrl')
+const { signUp, login, getAll, gets, updateIntern, deleteIntern, updatePassword, handleRefreshToken,resetnewpassword, resetpassword, forgotpassword } = require('../controller/internCntrl')
 
 
 router.post('/signUp',signUp)
 
 router.post('/login' , login)
 
+router.post('/forgot-password-token',forgotpassword)
+
 router.get('/get' ,getAll)
 
 router.get('/:id' , gets)
+
+router.get('/reset-password',resetpassword)
 
 router.put('/update/:id', updateIntern)
 
@@ -18,6 +22,7 @@ router.put('/refresh' , handleRefreshToken)
 
 router.put('/updatePassword/:id' ,updatePassword)
 
+router.post('/reset-password-token',resetnewpassword)
 
 router.delete('/:id' ,deleteIntern)
 
