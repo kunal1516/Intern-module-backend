c364c5b395a7bd2d63f555957d174979a072251b
kunal1516 <147371834+kunal1516@users.noreply.github.com>
2024-02-26 14:59:07 +0530

    update

--- router/teamRoute.js
+++ router/teamRoute.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const {addTeam, getTeam, getAll, deleteTeam, updateTeam}=require('../controller/teamCtrl');
-const { saveTeamImage } = require('../middleware/uploadImages');
+const {addTeam, getTeam, getAll, deleteTeam, updateTeam}=require('../controller/teamCtrl')
+const { saveTeamImage } = require('../middleware/uploadImages')
 
 router.post('/add' , saveTeamImage,addTeam)
 router.get('/:id',getTeam)
 router.get('/',getAll)
 router.delete('/:id',deleteTeam)
-router.put('/:id',updateTeam)
+router.put('/:id', saveTeamImage, updateTeam)
 
 module.exports=router;
