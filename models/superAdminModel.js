b751cb612a09d1df21491181f690ee8863d5d6a8
SnehaPardeshi <147605176+SnehaPardeshi@users.noreply.github.com>
2024-03-03 11:43:00 +0530

    update

---
+++ models/superAdminModel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const crypto = require("crypto");
+
+var superAdminSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    mobile:{
+        type: Number,
+        required:true,
+    },
+    password:{
+        type:String,
+        required:true,
+    },
+    passwordResetToken: String,  // Added field for password reset token
+    passwordResetExpires: Date   // Added field for password reset expiration
+});
+
+superAdminSchema.pre('save', async function (next){
+    if(!this.isModified('password')) {
+        next()
+    }
+    const salt = await bcrypt.genSaltSync(10)
+    this.password = await bcrypt.hash(this.password, salt)
+})
+
+superAdminSchema.methods.isPasswordMatched = async function(enteredPassword){
+    return await bcrypt.compare(enteredPassword, this.password)
+}
+
+superAdminSchema.methods.createPasswordResetToken = async function () {
+    const resetToken = crypto.randomBytes(32).toString("hex")
+    this.passwordResetToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex')
+    this.passwordResetExpires = Date.now() + 30 *60 * 1000  // 30 minutes
+    return resetToken
+}
+
+module.exports = mongoose.model('superAdmin', superAdminSchema);
