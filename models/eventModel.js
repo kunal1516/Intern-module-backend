cc63c4e69c3a6f1eb75b6d3af7400ecf740b75d1
SakshiKadave <115137353+Sakshikadave@users.noreply.github.com>
2024-03-04 17:37:25 +0530

    update

--- models/eventModel.js
+++ models/eventModel.js
@@ -1,35 +1,32 @@
-const mongoose = require('mongoose'); // Erase if already required
+const mongoose = require("mongoose"); // Erase if already required
 
 // Declare the Schema of the Mongo model
 var eventSchema = new mongoose.Schema({
-  
-    title:{
-        type:String,
-        required:true,
-       
-    },
-    description:{
-        type:String,
-        required:true,
-        
-    },
-    image: {
-        type:String,
-        required : true
-    },
-    startDate:{
-        type:Date,
-        required:true,
-    },
-    endDate:{
-        type:Date,
-        required:true,
-    },
-    location:{
-        type: String,
-        required:true,
-    },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+  },
+  startDate: {
+    type: Date,
+    required: true,
+  },
+  endDate: {
+    type: Date,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
 });
 
 //Export the model
-module.exports = mongoose.model('Event', eventSchema);
+module.exports = mongoose.model("Event", eventSchema);
